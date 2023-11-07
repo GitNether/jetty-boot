e522c7b581f29e32b7c4473281acf4442341b0e7
Nether <trextim@gmx.de>
2023-11-07 22:46:59 +0100

    initial commit with template

---
+++ bin/launch-playground.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/trext/OneDrive/Desktop/THU/Semester 5/CG/2D-Project/playground/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\trext\OneDrive\Desktop\THU\Semester 5\CG\2D-Project\bin\playground.exe"  
+pause

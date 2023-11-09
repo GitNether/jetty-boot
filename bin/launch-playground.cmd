81dd58324bf364540e6fd94c1cdb4368d6804dc0
Nether <trextim@gmx.de>
2023-11-09 10:51:44 +0100

    reset to java

--- bin/launch-playground.cmd
+++
@@ -1,9 +0,0 @@
-cd C:/Users/trext/OneDrive/Desktop/THU/Semester 5/CG/2D-Project/playground/
-set 
-
-if [%1]==[--debugger] (
-	SHIFT
-)
-
-"C:\Users\trext\OneDrive\Desktop\THU\Semester 5\CG\2D-Project\bin\playground.exe"  
-pause
